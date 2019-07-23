388fda67b4b54ae12e2f39a126d473579e78aa28
max delzer <emma.delzer@gmail.com>
2019-07-23 16:51:58 -0700

    Created folder architecture and files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name) VALUES ('Ground Chicken Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Greek Burger', false);
+
