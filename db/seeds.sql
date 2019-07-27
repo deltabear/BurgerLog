da31da0a1d59da7b278c516398925699644eb5a8
max delzer <emma.delzer@gmail.com>
2019-07-26 22:13:43 -0700

    fix- did not specify devoured value.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name) VALUES ('Ground Chicken Burger', false);
-INSERT INTO burgers (burger_name) VALUES ('Greek Burger', false);
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ground Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Greek Burger', false);
 
