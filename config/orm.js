13949802a826fac49e9abe5c2da9d44dc3ec7696
max delzer <emma.delzer@gmail.com>
2019-07-26 22:14:58 -0700

    Fix- devoured default false, burger_name not null

--- config/orm.js
+++ config/orm.js
@@ -4,86 +4,102 @@
 //updateOne()
 
 //Export the ORM object in module.exports.
+//===========
 
 // Import MySQL connection.
 var connection = require("../config/connection");
 
 
-function createQmarks(num) {
-  var arr = [];
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-  return arr.toString();
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+
+	return arr.toString();
 }
 
 // translates sql into a readable query
-function translateSql(ob) {
-  var arr = [];
-  for (var key in ob) {
-    var value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
-    }
-  }
-  return arr.toString();
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		arr.push(key + "=" + ob[key]);
+	}
+
+	return arr.toString();
 }
 
+
+//selectAll()
 var orm = {
-  selectAll: function(table, cb) {
-    var dbQuery = "SELECT * FROM " + table + ";";
+	// Function that returns all table entries
+	selectAll: function(tableInput, cb) {
+		// Construct the query string that returns all rows from the target table
+		var queryString = "SELECT * FROM " + tableInput + ";";
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
 
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+  //insertOne()
   // Inserts single table entry
   insertOne: function(table, cols, vals, cb) {
-    var dbQuery =
-      "INSERT INTO " +
-      table +
-      " (" +
-      cols.toString() +
-      ") " +
-      "VALUES (" +
-      createQmarks(vals.length) +
-      ") ";
+		// Construct the query string that inserts a single row into the target table
+		var queryString = "INSERT INTO " + table;
 
-    console.log(dbQuery);
-    connection.query(dbQuery, vals, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
 
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+  
+//updateOne()
   // Updates single table entry
   updateOne: function(table, objColVals, condition, cb) {
-    var dbQuery =
-      "UPDATE " +
-      table +
-      " SET " +
-      translateSql(objColVals) +
-      " WHERE " +
-      condition;
+		// Construct the query string that updates a single entry in the target table
+		var queryString = "UPDATE " + table;
 
-    console.log(dbQuery);
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  }
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	}
 };
 
 // Export the orm object for the model
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers(
 id INT NOT NULL auto_increment,
-burger_name VARCHAR(50),
-devoured BOOLEAN,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
